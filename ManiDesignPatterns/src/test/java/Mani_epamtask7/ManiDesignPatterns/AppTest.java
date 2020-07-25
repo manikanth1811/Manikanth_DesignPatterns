f093d65ffac6972cdecc4d45c59461de5a4acedc
manikanth18 <manikanthpolice18@gmail.com>
2020-07-25 19:43:43 +0530

    changes made

---
+++ ManiDesignPatterns/src/test/java/Mani_epamtask7/ManiDesignPatterns/AppTest.java
@@ -0,0 +1,20 @@
+package Mani_epamtask7.ManiDesignPatterns;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
